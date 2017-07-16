88cb5cf99952a38e067d072d495d1a9229ec8333
Dan Riches <driches@ymail.com>
2017-07-16 15:41:16 -0700

    working on the mobile version

---
+++ argentina/js/script.js
@@ -0,0 +1,10 @@
+function myFunction(nav) {
+    nav.classList.toggle("change");
+
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
